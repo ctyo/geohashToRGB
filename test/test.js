f8c4de606452e624081d1fbb849fa0eb95485ad9
ctyo <ctyo@live.jp>
2018-10-20 19:31:25 +0900

    アタック25が間違ってたので修正

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ var geohash = 'xn1rc';
 
 try {
     assert.strictEqual('rgb(191,85,7,1)', geohash.geohashToRGB());
-    assert.strictEqual('rgb(191,85,7,1)', geohash.geohashToAttack25());
+    assert.strictEqual('rgba(255,179,180,1)', geohash.geohashToAttack25());
     console.log('ok');
   } catch (err) {
     console.log(err);
