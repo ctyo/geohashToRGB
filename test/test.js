1b76c29c668426f84700babd1faacb51ad778502
ctyo <ctyo@live.jp>
2018-10-20 19:32:15 +0900

    透過のテストコード追加

--- test/test.js
+++ test/test.js
@@ -6,6 +6,10 @@ var geohash = 'xn1rc';
 try {
     assert.strictEqual('rgb(191,85,7,1)', geohash.geohashToRGB());
     assert.strictEqual('rgba(255,179,180,1)', geohash.geohashToAttack25());
+    // 透過
+    assert.strictEqual('rgb(191,85,7,0.5)', geohash.geohashToRGB(0.5));
+    assert.strictEqual('rgba(255,179,180,0.5)', geohash.geohashToAttack25(0.5));
+
     console.log('ok');
   } catch (err) {
     console.log(err);
